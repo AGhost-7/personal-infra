e6f5a489b6613fd6c60caf8f5f087c2e5140053f
AGhost-7 <jonathan.boudreau.92@gmail.com>
2018-01-03 22:16:10 -0500

    Add https

---
+++ cert.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$1" ]; then
+	echo -e '\033[0;31mEmail must be specified as the first parameter to this script\033[0;0m'
+	exit 1
+fi
+
+docker run -ti \
+	--rm \
+	-p 80:80 \
+	-p 443:443 \
+	-v /etc/letsencrypt:/etc/letsencrypt \
+	aghost7/certbot \
+	certbot certonly \
+		--standalone \
+		--agree-tos \
+		--no-eff-email \
+		--email "$1" \
+		-d www.jonathan-boudreau.com \
+		-d jonathan-boudreau.com \
+		-d chat.jonathan-boudreau.com
