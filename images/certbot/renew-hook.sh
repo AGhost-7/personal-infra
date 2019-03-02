d769542bc35d0c864610796454dd7ec3da76471b
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-03-02 12:41:15 -0500

    make certbot reload the load balancer

--- images/certbot/renew-hook.sh
+++ images/certbot/renew-hook.sh
@@ -6,3 +6,5 @@ for livedir in /etc/letsencrypt/live/*; do
         domain="$(basename "$livedir")"
         cat "$livedir"/{fullchain.pem,privkey.pem} > /etc/haproxy/certs/"$domain".pem
 done
+
+docker kill --signal=HUP "$LOAD_BALANCER_CONTAINER"
