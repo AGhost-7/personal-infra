e0ddd3c59e4e8294aaf244b95f0b8d61e56d7a30
AGhost-7 <jonathan-boudreau@protonmail.com>
2021-10-02 14:49:11 -0400

    update epic loot

--- roles/valheim/files/post-install.sh
+++ roles/valheim/files/post-install.sh
@@ -2,12 +2,21 @@
 
 set -exo pipefail
 
+epic_loot_version=0.8.6
 epic_loot_path=/home/steam/valheim/BepInEx/plugins/EpicLoot
+
+if ! grep -q "$epic_loot_version" "$epic_loot_path/version"; then
+	echo "Epic loot version is outdated, removing"
+	rm -rf "$epic_loot_path"
+fi
+
 if [ ! -d $epic_loot_path ]; then
-	curl -f -o /tmp/epicloot.zip https://cdn.thunderstore.io/live/repository/packages/RandyKnapp-EpicLoot-0.8.2.zip
+	echo "Installing epic loot"
+	curl -f -o /tmp/epicloot.zip "https://gcdn.thunderstore.io/live/repository/packages/RandyKnapp-EpicLoot-$epic_loot_version.zip"
 	mkdir -p /tmp/epicloot
 	unzip /tmp/epicloot.zip -d /tmp/epicloot
 	cp -r /tmp/epicloot/files "$epic_loot_path"
+	echo "$epic_loot_version" > "$epic_loot_version"
 	chown -R 1000:1000 "$epic_loot_path"
 fi
 
